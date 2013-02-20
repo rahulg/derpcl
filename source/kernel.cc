a42eb2e4f0c167ab7c16cede84ab713bc7729148
Rahul AG <r@hul.ag>
2013-02-20 18:27:04 +0800

    Added Kernel class.

---
+++ source/kernel.cc
@@ -0,0 +1,69 @@
+#include "kernel.h"
+
+using namespace curt;
+
+Kernel::Kernel(Program& program, string name) throw(string) : _program(program) {
+
+	cl_int error;
+
+	_kernel = clCreateKernel(_program.program(), name.c_str(), &error);
+	if (error != CL_SUCCESS) {
+		throw string("Error creating kernel: ") + to_string(error);
+	}
+
+}
+
+Kernel::~Kernel() {
+	clReleaseKernel(_kernel);
+}
+
+cl_event Kernel::run(size_t units) throw(string) {
+	return _run(_program.environment().queue(), 1, &units, 0, nullptr);
+}
+
+cl_event Kernel::run(size_t x, size_t y) throw(string) {
+	size_t dimensions[2] = { x, y };
+	return _run(_program.environment().queue(), 2, dimensions, 0, nullptr);
+}
+
+cl_event Kernel::run(size_t units, cl_event wait_event) throw(string) {
+	return _run(_program.environment().queue(), 1, &units, 1, &wait_event);
+}
+
+cl_event Kernel::run(size_t x, size_t y, cl_event wait_event) throw(string) {
+	size_t dimensions[2] = { x, y };
+	return _run(_program.environment().queue(), 2, dimensions, 1, &wait_event);
+}
+
+cl_event Kernel::run(DeviceType device_type, size_t units) throw(string) {
+	return _run(_program.environment().queue(), 1, &units, 0, nullptr);
+}
+
+cl_event Kernel::run(DeviceType device_type, size_t x, size_t y) throw(string) {
+	size_t dimensions[2] = { x, y };
+	return _run(_program.environment().queue(), 2, dimensions, 0, nullptr);
+}
+
+cl_event Kernel::run(DeviceType device_type, size_t units, cl_event wait_event) throw(string) {
+	return _run(_program.environment().queue(), 1, &units, 1, &wait_event);
+}
+
+cl_event Kernel::run(DeviceType device_type, size_t x, size_t y, cl_event wait_event) throw(string) {
+	size_t dimensions[2] = { x, y };
+	return _run(_program.environment().queue(), 2, dimensions, 1, &wait_event);
+}
+
+cl_event Kernel::_run(cl_command_queue queue, size_t n_dim, size_t* dimensions, size_t n_wait, cl_event* wait_event) throw(string) {
+
+	cl_int error;
+	cl_event run_event;
+
+	error = clEnqueueNDRangeKernel(queue, _kernel, n_dim, nullptr, dimensions, nullptr, n_wait, wait_event, &run_event);
+	if (error != CL_SUCCESS) {
+		throw string("Error running kernel: ") + to_string(error);
+	}
+
+	return run_event;
+
+}
+
---
+++ source/kernel.h
@@ -0,0 +1,95 @@
+/*!
+ * @file curt/kernel.h
+ * Definiton of the curt kernel class.
+ *
+ * @author Rahul A. G.
+ */
+
+#ifndef __CURT_KERNEL_H__
+#define __CURT_KERNEL_H__
+
+#include "common.h"
+#include "env.h"
+#include "buffer.h"
+#include "image.h"
+#include "program.h"
+
+#include <ostream>
+
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+using namespace std;
+
+namespace curt {
+
+	class Kernel {
+
+	public:
+
+		Kernel(Program& program, string name) throw(string);
+		~Kernel();
+
+		template <class T>
+		void setArgument(int position, T const& argument) throw(string) {
+
+			cl_int error;
+
+			error = clSetKernelArg(_kernel, position, sizeof(T), &argument);
+			if (error != CL_SUCCESS) {
+				throw string("Error setting kernel argument: ") + to_string(error);
+			}
+
+		}
+
+		template <class Tdata>
+		void setArgument(int position, Buffer<Tdata> const& argument) {
+
+			cl_int error;
+
+			error = clSetKernelArg(_kernel, position, sizeof(cl_mem), &argument.block());
+			if (error != CL_SUCCESS) {
+				throw string("Error setting kernel argument: ") + to_string(error);
+			}
+
+		}
+
+		// template <class Tdata>
+		// void setArgument(int position, Image<Tdata> const& argument) {
+
+		// 	cl_int error;
+
+		// 	error = clSetKernelArg(_kernel, position, sizeof(cl_mem), &argument.block());
+		// 	if (error != CL_SUCCESS) {
+		// 		throw string("Error setting kernel argument: ") + to_string(error);
+		// 	}
+
+		// }
+
+		cl_event run(size_t units) throw(string);
+		cl_event run(size_t x, size_t y) throw(string);
+
+		cl_event run(size_t units, cl_event wait_event) throw(string);
+		cl_event run(size_t x, size_t y, cl_event wait_event) throw(string);
+
+		cl_event run(DeviceType device_type, size_t units) throw(string);
+		cl_event run(DeviceType device_type, size_t x, size_t y) throw(string);
+
+		cl_event run(DeviceType device_type, size_t units, cl_event wait_event) throw(string);
+		cl_event run(DeviceType device_type, size_t x, size_t y, cl_event wait_event) throw(string);
+
+	private:
+
+		cl_event _run(cl_command_queue queue, size_t n_dim, size_t* dimensions, size_t n_wait, cl_event* wait_event) throw(string);
+
+		Program& _program;
+
+		cl_kernel _kernel;
+
+	};
+
+}
+
+#endif
